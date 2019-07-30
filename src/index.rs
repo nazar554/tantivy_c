987b60a25fdd02a82ed14354ed0f880b0980cefa
Nazar Mishturak <nazarmx@gmail.com>
2019-07-31 00:16:59 +0300

    Index threading

--- src/index.rs
+++ src/index.rs
@@ -9,3 +9,21 @@ pub unsafe extern "C" fn tantivy_index_create_in_ram(schema: *mut Schema) -> *mu
 
     box_new_into_raw!(Index::create_in_ram(*schema))
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn tantivy_index_set_multithread_executor(
+    index: *mut Index,
+    num_threads: usize,
+) {
+    (&mut *index).set_multithread_executor(num_threads)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tantivy_index_set_default_multithread_executor(index: *mut Index) {
+    (&mut *index).set_default_multithread_executor()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tantivy_index_schema(index: *const Index) -> *mut Schema {
+    box_new_into_raw!((&*index).schema())
+}
